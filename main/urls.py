2fcc2b4e06c7fd96e65699bf1751535f4f34f07a
Khoshanlo Sharip <khoshanlosharip@gmail.com>
2021-04-05 14:34:21 +0600

    "for beks"

--- main/urls.py
+++ main/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('',index,name='home')
+    path('',index,name='home'),
+    path('study/',study,name='study'),
+    path('blog/',blog,name='blog')
 
 ]
--- main/views.py
+++ main/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request,'index.html')
+
+def study(request):
+    return render(request,'study.html')
+
+def blog(request):
+    return render(request,'blog.html')
